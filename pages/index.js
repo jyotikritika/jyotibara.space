2de02bd64d7d7095caaab933148ad182aae964f7
Jyoti Kritika Bara <35219316+jyotikritika@users.noreply.github.com>
2021-12-19 13:45:53 +0530

    Landing page

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,15 @@
-
 export default function Home() {
+  const statement = {
+    height: "100vh",
+    backgroundColor: "black",
+    color: "white",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center"
+  };
   return (
-    <div>
-      Hello I am Jyoti!
+    <div style={statement}>
+      <h2>Hello I am Jyoti Kritika Bara.</h2>
     </div>
-  )
+  );
 }
